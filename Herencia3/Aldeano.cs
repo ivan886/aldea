7e58723625283a2cf61104c2b6a0309cc6f809ed
ivan <ivanfernando.fonseca@gmail.com>
2021-09-28 10:43:36 -0500

    Manejo de herencia

--- Herencia3/Aldeano.cs
+++ Herencia3/Aldeano.cs
@@ -1,7 +1,22 @@
-﻿namespace Herencia3
+﻿using System;
+
+namespace Herencia3
 {
-    public class Aldeano
+    public class Aldeano:Habitante
     {
-        
+        public Aldeano(string nombre, char genero, DateTime fechaNacimiento, DateTime fechaFin, string mestizaje) : base(nombre, genero, fechaNacimiento, fechaFin, mestizaje)
+        {
+        }
+
+        public string cultivar()
+        {
+            return "estoy cultivando";
+        }
+
+        public string edificar()
+        {
+            return "estoy edificando";
+        }
+
     }
 }
--- Herencia3/Program.cs
+++ Herencia3/Program.cs
@@ -6,11 +6,20 @@ namespace Herencia3
     {
         static void Main(string[] args)
         {
+            Aldeano unAldeano = new Aldeano( "Marcos", 'm', new DateTime(1700,02,12 ),new DateTime(1700,02,12 ),"blanco"  );
             Guerrero unGuerrero = new Guerrero("Furia nocturna", "Jaime", 'm', new DateTime(1700,02,12 ),new DateTime(1700,02,12 ),"blanco"  );
             
+            Console.WriteLine("CARACTERISTICAS ALDEANO ");
+            Console.WriteLine(unAldeano.Nombre);
+            Console.WriteLine(unAldeano.caminar());
+            Console.WriteLine(unAldeano.correr());
+            Console.WriteLine(unAldeano.cultivar());
+            
+            Console.WriteLine("CARACTERISTICAS GUERRERO ");
             Console.WriteLine(unGuerrero.Nombre);
             Console.WriteLine(unGuerrero.caminar());
             Console.WriteLine(unGuerrero.correr());
+            Console.WriteLine(unGuerrero.pelear());
         }
     }
 }
