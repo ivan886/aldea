824a17301f404606d64792fc43e90d04fb26aba6
ivan <ivanfernando.fonseca@gmail.com>
2021-10-01 10:01:54 -0500

    ciclo repetitivo para identificar un aldeano de un guerrero

--- Herencia3/Aldea.cs
+++ Herencia3/Aldea.cs
@@ -2,9 +2,40 @@
 {
     public class Aldea
     {
+        private string nombre;
+        private string ubicacion;
         public Habitante[] habitantes = new Habitante[100];
+
+     
+
+        public Aldea(string nombre, string ubicacion)
+        {
+            this.nombre = nombre;
+            this.ubicacion = ubicacion;
+        }
+
+        public string getNombre()
+        {
+            return this.nombre;
+        }
+
+        public void setNombre(string nombre)
+        {
+            this.nombre = nombre;
+        }
+
+        public string getUbicacion()
+        {
+            return this.ubicacion;
+        }
+
+        public void setUbicacion(string ubicacion)
+        {
+            this.ubicacion = ubicacion;
+        }
         
-        
-        
+
+
+
     }
 }
--- Herencia3/Program.cs
+++ Herencia3/Program.cs
@@ -6,35 +6,34 @@ namespace Herencia3
     class Program
     {
         static void Main(string[] args)
-        {
+        {                             ///instanciar
             Aldeano unAldeano = new Aldeano( "Eynar", 'm', new DateTime(1700,02,12 ),new DateTime(1700,02,12 ),"blanco"  );
             Guerrero unGuerrero = new Guerrero("Furia nocturna", "Marcos", 'm', new DateTime(1700,02,12 ),new DateTime(1700,02,12 ),"blanco"  );
-            Guerrero dosGuerrero = new Guerrero("Furia nocturna", "Jaime", 'm', new DateTime(1700,02,12 ),new DateTime(1700,02,12 ),"blanco"  );
-            Aldea unaAldea = new Aldea();
+            Guerrero dosGuerrero = new Guerrero("William ", "Jaime", 'm', new DateTime(1700,02,12 ),new DateTime(1700,02,12 ),"blanco"  );
+            Aldea unaAldea = new Aldea("la aldea", "calle falsa 123 ");
 
             unaAldea.habitantes[0] = unAldeano;
             unaAldea.habitantes[1] = unGuerrero; 
             unaAldea.habitantes[2] = dosGuerrero;
 
            /*Imprimir la informacion de los diferentes habitantes*/
-            foreach (Habitante obj in unaAldea.habitantes)
-            {  Console.WriteLine(obj.Nombre);
-                
+            foreach (Habitante unHabitante in unaAldea.habitantes)
+            {  Console.WriteLine(unHabitante.Nombre);
                 /*el objeto es un habitante  no podemos acceder a
                  funcionalidades especificas de acuerd a su tipo guerrero o aldeano 
                  para esto debemos conocer de que tipo de objeto se trata  
                  para realizar un cast y acceder a sus metodos y atributos caracteristicos*/
-                if (obj.GetType().Name.Equals("Guerrero"))
+                if (unHabitante.GetType().Name.Equals("Guerrero"))
                 { Console.WriteLine("Soy un Guerrero");
                    
-                    var guerrero = (Guerrero) obj;    
-                    
-                    guerrero.pelear();
+                    var guerrero = (Guerrero) unHabitante;    
+                    Console.WriteLine(guerrero.pelear());
+                    Console.WriteLine( guerrero.defender());
                 }
                 else
-                {var aldeano = (Aldeano) obj;
+                {  var aldeano = (Aldeano) unHabitante;
                     Console.WriteLine("Soy un Aldeano");
-                    aldeano.cultivar();
+                    Console.WriteLine(aldeano.cultivar());
                 }
             
                 
